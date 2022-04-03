c3b38017c3951e23981e02765c1de94396e00dc0
Andrew Huntington <andrew.huntington@gmail.com>
2022-04-03 11:10:10 +0900

    Create Details component. React Router Route

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import { StrictMode } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import SearchParams from "./SearchParams";
+import Details from "./Details";
 
+// TODO: Try to upgrade to React Router 6 (new branch)
 const App = () => {
   return (
     <div>
       <h1>Adopt Me!</h1>
-      <SearchParams />
+      <Router>
+        <Switch>
+          <Route path="/details/:id">
+            <Details />
+          </Route>
+          <Route path="/">
+            <SearchParams />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 };
---
+++ src/Details.js
@@ -0,0 +1,5 @@
+const Details = () => {
+  return <h2>hello world</h2>;
+};
+
+export default Details;
